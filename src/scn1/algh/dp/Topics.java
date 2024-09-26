9b55c3310269638587a553befae5a02e819e6c67
SNL1CNG <fixed-term.Lumeng.SUN@cn.bosch.com>
2024-09-26 16:49:29 +0800

    零钱兑换 DP

--- src/scn1/algh/dp/Topics.java
+++ src/scn1/algh/dp/Topics.java
@@ -125,20 +125,36 @@ public class Topics {
     public int numDistinct(String s, String t) {
         int m = s.length();
         int n = t.length();
-        int[][] dp = new int[m+1][n+1];
-        for(int i=1;i<=m;i++){
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
             dp[i][0] = 1;
         }
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=Math.min(i,n);j++){
-                if(s.charAt(i) == t.charAt(j)){
-                    dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
-                }else {
-                    dp[i][j] = dp[i-1][j];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= Math.min(i, n); j++) {
+                if (s.charAt(i) == t.charAt(j)) {
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-    return dp[m][n];
+        return dp[m][n];
+    }
+
+    /**
+     * TODO 零钱兑换
+     */
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+
     }
 
     public static void main(String[] args) {
